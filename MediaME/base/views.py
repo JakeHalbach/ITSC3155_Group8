750c53567503e2d5fa62ce6c83a6abf920fe1c31
Abarri20 <ambarrie03@gmail.com>
2025-03-27 13:11:27 -0400

    Co-authored-by: Jake_Halbach <JakeHalbach@users.noreply.github.com> Co-authored-by: Yameen Alsaaidah <Yameen05@users.noreply.github.com> Co-authored-by: Stephanie Ijiogbe <suijiogbe@users.noreply.github.com> Co-authored-by: cgriff92 <cgriff92@users.noreply.github.com>

--- MediaME/base/views.py
+++ MediaME/base/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from .models import media
 # Create your views here.
+
+def home(request):
+    context = {}
+    return render(request, 'base/home.html', context)
+
+def titlePage(request, pk):
+    title= media.objects.get(id=pk)
+    context = {'title':title}
+    return render(request, 'base/titlePage.html', context)
+
+
