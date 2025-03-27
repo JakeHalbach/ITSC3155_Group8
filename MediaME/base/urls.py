033f250a9e5631edef2e95df23e239a30be455fd
Abarri20 <ambarrie03@gmail.com>
2025-03-27 13:11:27 -0400

    Co-authored-by: Jake_Halbach <JakeHalbach@users.noreply.github.com> Co-authored-by: Yameen Alsaaidah <Yameen05@users.noreply.github.com> Co-authored-by: Stephanie Ijiogbe <suijiogbe@users.noreply.github.com> Co-authored-by: cgriff92 <cgriff92@users.noreply.github.com>

--- MediaME/base/urls.py
+++ MediaME/base/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('titlePage/<str:pk>/',views.titlePage, name="title-page"),
 ]
--- MediaME/base/views.py
+++ MediaME/base/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'base/home.html')
+def titlePage(request, pk):
+    title= media.objects.get(id=pk)
+    context = {'title':title}
+    return render(request, 'base/titlePage.html', context)
 
