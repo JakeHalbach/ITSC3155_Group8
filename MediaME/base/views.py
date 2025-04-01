de6ad66e831ff43c6aaa0fae3d1d666215de3d9f
Stephanie Ijiogbe <sijiogbe@charlotte.edu>
2025-04-01 13:52:07 -0400

    updating urls and views

--- MediaME/base/views.py
+++ MediaME/base/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
 
-def home(request):
-    return render(request, 'base/home.html')
+def impression(request):
+    return render(request, 'base/impression.html')
 
